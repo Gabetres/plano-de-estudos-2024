7836fed1fe09722bd249fb6de9655e9037a5d327
Gabetres <gabetres@hotmail.com>
2024-10-17 08:14:49 -0300

    Refactor code to improve type safety and readability

--- organizando-codigo-php/screen-match.php
+++ organizando-codigo-php/screen-match.php
@@ -1,16 +1,18 @@
 <?php
 
-function exibeMensagemLancamento($ano){
+function exibeMensagemLancamento(int $ano): void
+{
     if ($ano > 2022) {
         echo "Esse filme é um lançamento\n";
-    } elseif($ano > 2020 && $ano <= 2022) {
+    } elseif ($ano > 2020 && $ano <= 2022) {
         echo "Esse filme ainda é novo\n";
     } else {
         echo "Esse filme não é um lançamento\n";
     }
 }
 
-function incluidoNoPlano($planoPrime, $anoLancamento){
+function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool
+{
     return $planoPrime || $anoLancamento < 2020;
 }
 
