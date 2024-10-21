1f8d4ba68706ad728bbb817909e0a306a8081fb1
Gabetres <gabetres@hotmail.com>
2024-10-21 15:33:56 -0300

    Refactor code to calculate and deduct transaction fee when withdrawing from an account

---
+++ php-poo-advanced/teste-saque.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'autoload.php';
+
+use Alura\Banco\Model\Conta\Conta;
+use Alura\Banco\Model\Conta\Titular;
+use Alura\Banco\Model\Endereco;
+use Alura\Banco\Model\CPF;
+
+
+
+$conta = new Conta(
+    new Titular(
+        new CPF('123.456.789-10'),
+        'Vinicius Dias',
+        new Endereco('Petrópolis', 'bairro Teste', 'Rua lá', '37')
+    )
+);
+
+$conta->deposita(500);
+$conta->saca(100);
+
+echo $conta->recuperaSaldo();
+
+
