44e612f49269bd4aade12df6ac88fe9a9f7a01af
Gabetres <gabetres@hotmail.com>
2024-11-18 10:15:29 -0300

    Implement authentication system with Autenticavel interface and Autenticador service

--- php-poo-advanced/src/Model/Funcionario/Gerente.php
+++ php-poo-advanced/src/Model/Funcionario/Gerente.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
 
 namespace Alura\Banco\Model\Funcionario;
 
-class Gerente extends Funcionario
+use Alura\Banco\Model\Autenticavel;
+
+class Gerente extends Funcionario implements Autenticavel
 {
     public function calculaBonificacao(): float
     {
         return $this->recuperaSalario();
+    }
+
+    public function podeAutenticar(string $senha): bool
+    {
+        return $senha === '4321';
     }
 }
