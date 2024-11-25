f22e5ac63f1061f1006a60f0fcb0beb1c2abb766
Gabetres <gabetres@hotmail.com>
2024-11-25 15:33:31 -0300

    ex007

---
+++ exercises/ex007/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$baseDoRetangulo = 15;
+$alturaDoRetangulo = 12;
+$areaDoRetangulo = $baseDoRetangulo * $alturaDoRetangulo;
+
+echo "A base do retangulo é: {$baseDoRetangulo}\n";
+echo "A altura do retangulo é: {$alturaDoRetangulo}\n";
+echo "A area do retangulo é: {$areaDoRetangulo}\n";
