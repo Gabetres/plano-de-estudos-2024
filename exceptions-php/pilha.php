2e1baaad60d389c0ba8c8e3fb4c24444a143d251
Gabetres <gabetres@hotmail.com>
2024-12-03 09:42:20 -0300

    Add error handling in funcao1 and fix array access in funcao2

--- exceptions-php/pilha.php
+++ exceptions-php/pilha.php
@@ -3,16 +3,28 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $error) {
+        echo $error->getMessage() . PHP_EOL;
+        echo $error->getLine() . PHP_EOL;
+        echo $error->getTraceAsString() . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
+
+    $divisao = intdiv(5, 0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'valor';
+
     echo 'Entrei na função 2' . PHP_EOL;
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
