b8fdc3782f0c89fc8808f2c000e756fe99f45470
Gabetres <gabetres@hotmail.com>
2024-12-03 17:24:27 -0300

    Remove redundant exception throwing in funcao1 and adjust output in funcao2

--- exceptions-php/pilha.php
+++ exceptions-php/pilha.php
@@ -5,18 +5,19 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
     try {
         funcao2();
-    } catch (RuntimeException | DivisionByZeroError $error) {
-        echo $error->getMessage() . PHP_EOL;
-        echo $error->getLine() . PHP_EOL;
-        echo $error->getTraceAsString() . PHP_EOL;
+    } catch (Throwable $e) {
+        echo $e->getMessage() . PHP_EOL;
+        echo $e->getLine() . PHP_EOL;
+        echo $e->getTraceAsString() . PHP_EOL;
     }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
-    echo 'Entrei na função 2';
+    echo 'Entrei na função 2' . PHP_EOL;
 
+    intdiv(1, 0);
     $exception = new RuntimeException();
     throw $exception;
 
